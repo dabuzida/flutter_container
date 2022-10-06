ff1c6fde567dff693111d4996f5926520db4c31b
dabuzida <dabuzida@gmail.com>
2022-10-06 14:36:44 +0900

    gesture detector, mouse region 차이 실험

--- lib/container1.dart
+++ lib/container1.dart
@@ -10,15 +10,45 @@ class Container1 extends StatefulWidget {
 }
 
 class _Container1State extends State<Container1> {
+  Color x = Colors.green;
+  Color y = Colors.red;
+  Color? z;
+  @override
+  void initState() {
+    super.initState();
+    z = x;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        // decoration: BoxDecoration(border: Border.all(color: Colors.green, width: 1)),
-        color: Colors.red,
-        // width: 100,
-        // height: 100,
-        child: Text('dddddddddd'),
+      child: GestureDetector(
+        onTap: () {
+          if (z == x) {
+            z = y;
+          } else {
+            z = x;
+          }
+          setState(() {});
+        },
+        child: MouseRegion(
+          // cursor: SystemMouseCursors.click,
+          cursor: MouseCursor.defer,
+          child: Container(
+            width: 300,
+            height: 300,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.green,
+                width: 1,
+              ),
+              color: z,
+            ),
+            // width: 100,
+            // height: 100,
+            child: Text('dddddddddd'),
+          ),
+        ),
       ),
     );
   }
