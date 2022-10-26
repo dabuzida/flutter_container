bcaa7ebd5da3804a76bd641b282d39d98e0966f7
dabuzida <dabuzida@gmail.com>
2022-10-26 18:05:29 +0900

    1

--- lib/container2.dart
+++ lib/container2.dart
@@ -8,27 +8,66 @@ class Container2 extends StatefulWidget {
 }
 
 class _Container2State extends State<Container2> {
+  final Color color1 = Colors.pinkAccent; //
+  final Color color2 = Colors.tealAccent[700]!;
+  late Color _color;
+  bool _switch = true;
+  @override
+  void initState() {
+    super.initState();
+    _color = color1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
+    print('build');
+    return Row(
       children: <Widget>[
-        // Container(width: 50, height: 50, color: Colors.amber),
-        SafeArea(
-          // minimum: const EdgeInsets.all(32.0),
-          left: true,
-          child: Container(
-            width: 600,
-            height: 50,
-            constraints: BoxConstraints(
-                // minWidth: 600,
-                ),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.red, width: 5),
-              color: Colors.amber,
-            ),
+        Container(
+          width: 600,
+          height: 600,
+          color: Colors.amber,
+          child: a(),
+        ),
+        TextButton(
+          onPressed: () {
+            if (_switch) {
+              _color = color2;
+              _switch = !_switch;
+            } else {
+              _color = color1;
+              _switch = !_switch;
+            }
+            setState(() {});
+          },
+          child: Text(
+            '색깔변경',
           ),
         ),
       ],
+    );
+  }
+
+  Widget a() {
+    print('a');
+    return Center(
+      child: Container(
+        width: 500,
+        height: 500,
+        color: _color,
+        child: b(),
+      ),
+    );
+  }
+
+  Widget b() {
+    print('b');
+    return Center(
+      child: Container(
+        width: 300,
+        height: 300,
+        color: Colors.blue,
+      ),
     );
   }
 }
