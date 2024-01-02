e4952bb3934bbf563c245d94d2baaa68524da045
dabuzida <dabuzida@gmail.com>
2024-01-02 15:44:17 +0900

    test

--- lib/container4.dart
+++ lib/container4.dart
@@ -10,21 +10,31 @@ class Container4 extends StatefulWidget {
 class _Container4State extends State<Container4> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        decoration: BoxDecoration(border: Border.all(color: Colors.blue)),
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.pink,
-              width: 10,
-            ),
-          ),
-          padding: EdgeInsets.all(30),
-          margin: EdgeInsets.all(40),
-          child: Container(width: 300, height: 300, color: Colors.amber),
-        ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        _xx(Colors.amber),
+        _xx(Colors.orange),
+        _xx(Colors.deepOrange),
+        _xx(Colors.orangeAccent),
+        _xx(Colors.teal),
+        _xx(Colors.purple),
+        _xx(Colors.deepPurpleAccent),
+        _xx(Colors.deepPurple),
+      ],
+    );
+  }
+
+  Widget _xx(Color color) {
+    return Container(
+      width: 700,
+      height: 50,
+      padding: const EdgeInsets.only(top: 100),
+      decoration: BoxDecoration(
+        color: color,
+        // border: Border.all(color: Colors.red),
       ),
+      child: SizedBox(),
     );
   }
 }
